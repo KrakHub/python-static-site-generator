29ff28c20c221306e76dfe42defd592cc3bf3ef8
Antonio Cotto <superajc@outlook.com>
2022-10-04 12:04:35 -0400

    Code up to 'Open file for writing'

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,4 +2,19 @@ from typing import List
 from pathlib import Path
 
 class Parser:
-    extensions = []
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+    
+    def parse(path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+    
+    def read(path):
+        with open(path) as file:
+            return file.read()
+    
+    def write(self, path, dest, content, ext = ".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path) as file:
+            return file.write(content)
