eba96937c00c9c2469e86f8859877b550f50e90a
Antonio Cotto <superajc@outlook.com>
2022-10-10 08:04:18 -0400

    (Probably) finished content class

--- ssg/content.py
+++ ssg/content.py
@@ -1,3 +1,4 @@
+from importlib.metadata import metadata
 from json import load
 import re
 from yaml import FullLoader
@@ -5,9 +6,45 @@ from yaml import load
 from collections.abc import Mapping
 
 class Content(Mapping):
-    def __init__(self):
-        __delimiter = r"^(?:-|\+){3}\s*$"
-        __regex = re.compile(__delimiter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
-    #def load(self, cls, string):
+    @classmethod
+    def load(self, cls, string):
+        _,fm,content = cls.__regex.split(string, 2)
+        metadata = load(fm,Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadta, content):
+        self.data = metadata
+        self.data["content"] = content
+    
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+            return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for value, key in self.data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
+
         
